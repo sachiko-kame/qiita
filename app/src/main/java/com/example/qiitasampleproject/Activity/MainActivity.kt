7b140f1099129ae4499cd67744131aa5d5ee0bc9
sachiko-kame <sachiko-kame@users.noreply.github.com>
2019-04-30 03:09:29 +0900

    調整

--- app/src/main/java/com/example/qiitasampleproject/Activity/MainActivity.kt
+++ app/src/main/java/com/example/qiitasampleproject/Activity/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.qiitasampleproject.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.transition.Fade
+import android.transition.TransitionManager
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import com.bumptech.glide.Glide
 import com.example.qiitasampleproject.Adapter.QiitaUserArticleAdapter
 import com.example.qiitasampleproject.Api.APIClient
 import com.example.qiitasampleproject.Api.Get.UserRepos
+import com.example.qiitasampleproject.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.concurrent.thread
 
@@ -36,8 +44,53 @@ class MainActivity : AppCompatActivity() {
             this.searchArticle(userName)
         }
 
-        serchResultListView.setOnItemClickListener {parent, view, position, id ->
+
+        serchResultListView.setOnItemClickListener { adapterView, view, position, id ->
             println(firstRepos[position].url)
+
+            val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val view = inflater.inflate(R.layout.popup_about_article,null)
+
+            val popupWindow = PopupWindow(
+                view,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                val fade = Fade()
+                fade.setDuration(1200);
+                popupWindow.enterTransition = fade
+                popupWindow.exitTransition = fade
+
+            }
+
+            val userImageView = view.findViewById<ImageView>(R.id.userImageView)
+            val followeesCountTextView = view.findViewById<TextView>(R.id.followeesCountTextView)
+            val followersCountTextView = view.findViewById<TextView>(R.id.followersCountTextView)
+            val idTextView = view.findViewById<TextView>(R.id.idTextView)
+
+            val closeButton = view.findViewById<Button>(R.id.closeButton)
+
+            val userImageUrl = firstRepos[position].user["profile_image_url"]
+            Glide.with(this).load(userImageUrl).into(userImageView);
+
+            followeesCountTextView.text = "フォローしている数 :" + firstRepos[position].user["followees_count"].toString()
+            followersCountTextView.text = "フォローされている数 : " + firstRepos[position].user["followers_count"].toString()
+            idTextView.text =  "id : " + firstRepos[position].user["id"].toString()
+
+
+            TransitionManager.beginDelayedTransition(this.serchResultListView)
+            popupWindow.showAtLocation(
+                this.serchResultListView, // Location to display popup window
+                Gravity.CENTER, // Exact position of layout to display popup
+                0, // X offset
+                0 // Y offset
+            )
+
+            closeButton.setOnClickListener{
+                popupWindow.dismiss()
+            }
         }
     }
 
@@ -92,3 +145,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
