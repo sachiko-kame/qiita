58421b378a4d3a9d054cdf823b09d09dc41937b3
sachiko-kame <sachiko-kame@users.noreply.github.com>
2019-04-30 17:35:34 +0900

    HTMLからテキストに変更するところの修正
    今後バージョンで使えないものになるものを使っていたので処理を分けました。

--- app/src/main/java/com/example/qiitasampleproject/Util/Extension/StringEx.kt
+++ app/src/main/java/com/example/qiitasampleproject/Util/Extension/StringEx.kt
@@ -1,13 +1,18 @@
 package com.example.qiitasampleproject.Util.Extension
 
-import android.text.Html.fromHtml
+import android.os.Build
+import android.text.Html
+
+
 
 fun String.removehtmlTag(): String{
-    return  fromHtml(this).toString().replace("\n", "")
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString().replace("\n", "")
+    } else {
+        Html.fromHtml(this).toString().replace("\n", "")
+    }
 }
 
 fun String.addText(text:String):String {
     return this + text
 }
-
-
