755dea23d85aaa16fd0dd4cfcd6bfd18fd4b2956
sachiko-kame <sachiko-kame@users.noreply.github.com>
2019-05-02 04:24:40 +0900

    記述場所の訂正と警告修正

--- app/src/main/java/com/example/qiitasampleproject/Activity/MainActivity.kt
+++ app/src/main/java/com/example/qiitasampleproject/Activity/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.transition.Fade
@@ -20,6 +21,7 @@ import com.example.qiitasampleproject.Api.APIClient
 import com.example.qiitasampleproject.Api.Get.UserRepos
 import com.example.qiitasampleproject.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 import kotlin.concurrent.thread
 
 
@@ -34,67 +36,73 @@ import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
     var firstRepos:List<UserRepos> = arrayListOf()
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(com.example.qiitasampleproject.R.layout.activity_main)
 
         serchButton.setOnClickListener {
-            val userName = searchUserNameTextView.text.toString()?: ""
+            val userName = searchUserNameTextView.text.toString()
             this.hideKeyboard()
             this.searchArticle(userName)
         }
 
 
         serchResultListView.setOnItemClickListener { adapterView, view, position, id ->
-            println(firstRepos[position].url)
-
-            val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val view = inflater.inflate(R.layout.popup_about_article,null)
-
-            val popupWindow = PopupWindow(
-                view,
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
-            )
-
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                val fade = Fade()
-                fade.setDuration(1200);
-                popupWindow.enterTransition = fade
-                popupWindow.exitTransition = fade
-
-            }
-
-            val userImageView = view.findViewById<ImageView>(R.id.userImageView)
-            val followeesCountTextView = view.findViewById<TextView>(R.id.followeesCountTextView)
-            val followersCountTextView = view.findViewById<TextView>(R.id.followersCountTextView)
-            val idTextView = view.findViewById<TextView>(R.id.idTextView)
-
-            val closeButton = view.findViewById<Button>(R.id.closeButton)
-
-            val userImageUrl = firstRepos[position].user["profile_image_url"]
-            Glide.with(this).load(userImageUrl).into(userImageView);
-
-            followeesCountTextView.text = "フォローしている数 :" + firstRepos[position].user["followees_count"].toString()
-            followersCountTextView.text = "フォローされている数 : " + firstRepos[position].user["followers_count"].toString()
-            idTextView.text =  "id : " + firstRepos[position].user["id"].toString()
-
-
-            TransitionManager.beginDelayedTransition(this.serchResultListView)
-            popupWindow.showAtLocation(
-                this.serchResultListView, // Location to display popup window
-                Gravity.CENTER, // Exact position of layout to display popup
-                0, // X offset
-                0 // Y offset
-            )
-
-            closeButton.setOnClickListener{
-                popupWindow.dismiss()
-            }
+            this.userPopupShow(firstRepos[position].user)
         }
     }
 
-    fun searchArticle(userName:String){
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
+    fun userPopupShow(userData: HashMap<String, Object>){
+
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.popup_about_article,null)
+
+        val popupWindow = PopupWindow(
+            view,
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val fade = Fade()
+            fade.duration = 1200
+            popupWindow.enterTransition = fade
+            popupWindow.exitTransition = fade
+
+        }
+
+        val userImageView = view.findViewById<ImageView>(R.id.userImageView)
+        val followeesCountTextView = view.findViewById<TextView>(R.id.followeesCountTextView)
+        val followersCountTextView = view.findViewById<TextView>(R.id.followersCountTextView)
+        val idTextView = view.findViewById<TextView>(R.id.idTextView)
+
+        val closeButton = view.findViewById<Button>(R.id.closeButton)
+
+        val userImageUrl = userData["profile_image_url"]
+        Glide.with(this).load(userImageUrl).into(userImageView)
+
+        followeesCountTextView.text = "フォローしている数 :" + userData["followees_count"].toString()
+        followersCountTextView.text = "フォローされている数 : " + userData["followers_count"].toString()
+        idTextView.text =  "id : " + userData["id"].toString()
+
+
+        TransitionManager.beginDelayedTransition(this.serchResultListView)
+        popupWindow.showAtLocation(
+            this.serchResultListView, // Location to display popup window
+            Gravity.CENTER, // Exact position of layout to display popup
+            0, // X offset
+            0 // Y offset
+        )
+
+        closeButton.setOnClickListener{
+            popupWindow.dismiss()
+        }
+
+    }
+
+    private fun searchArticle(userName:String){
 
         val handler = Handler()
 
@@ -132,12 +140,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun webOpen(url: String){
+    private fun webOpen(url: String){
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         startActivity(intent)
     }
 
-    fun hideKeyboard() {
+    private fun hideKeyboard() {
         if (currentFocus != null) {
             val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
